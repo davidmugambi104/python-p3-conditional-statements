5db7e95e5c4612bd7da9f34223b2fec3bbb98216
davidmugambi104 <davidmugambi104@gmail.com>
2024-05-20 15:33:06 +0300

    conditional-statements

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,55 @@
 
 def admin_login(username, password):
     # your code here
-    pass
-
+    if (username == "admin" or username == "ADMIN") and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+        
 def hows_the_weather(temperature):
-    # your code here
-    pass
+    if temperature < 40:
+        response = "brisk"
+    elif temperature >= 40 and temperature <= 65:
+        response = "a little chilly"
+    elif temperature > 85:
+        response = "too dang hot"
+    else:
+        response = "perfect"
+    
+    # Manually capitalize the first letter
+    response = response.capitalize()
+    
+    return f"It's {response} out there!"
 
+    
 def fizzbuzz(num):
     # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        num="FizzBuzz"
+
+    elif num % 3 == 0:
+        num="Fizz"
+
+    elif num % 5 == 0:
+        num="Buzz"
+    else:
+        print(num)
 
 def calculator(operation, num1, num2):
-    # your code here
-    pass
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            print("Cannot divide by zero!")
+            return None
+    else:
+        print("Invalid operation!")
+        return None
+
+        
